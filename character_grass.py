d2a9f4f65c42c2828dcd562eb16f9cd9384d9e4b
yoohalee@TIP209 <yooha0330@naver.com>
2024-09-24 11:56:17 +0900

    캐릭터 화면 출력 구현

--- character_grass.py
+++ character_grass.py
@@ -2,6 +2,30 @@ from pico2d import *
 
 open_canvas()
 
-# fill here
+grass = load_image('grass.png')
+boy = load_image('character.png')
+
+def run_circle():
+    print('CIRCLE')
+
+    clear_canvas_now()
+    boy.draw_now(400,300)
+    delay(0.1)
+    
+
+    pass ## 아무기능이 없는 빈 함수
+
+def run_rectangle():
+    print('RECTANGEL')
+    pass
+
+#file here
+
+while True:
+    run_circle()
+    run_rectangle()
+    break
+    
+
 
 close_canvas()
